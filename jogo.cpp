2a5b29d8e57bbee314d723b922c47420055e44a2
Vini1809 <vinicius.rocha.tarelho@gmail.com>
2026-01-15 09:27:03 -0300

    first commit

---
+++ jogo.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <random>
+#include <chrono>
+
+using namespace std;
+
+#define pathArquivo "dicionario.txt"
+#define tamanhoPalavra 6
+#define tamanhoPalavraMaxima 46
+#define qtdPalavraMaxima 20000
+
+
+int criarDicionario(char dicionario[][tamanhoPalavraMaxima], int tamPalavra)
+{
+    int quantPalavras = 0;
+    ifstream arquivo(pathArquivo);
+    char buffer[tamanhoPalavraMaxima];
+
+    if (arquivo.is_open()) 
+    {
+        while (arquivo.getline(buffer, tamanhoPalavraMaxima)) 
+        {
+            int tam;
+            for(tam = 0; buffer[tam] != '\0'; tam++) 
+            {
+                if (tam > tamPalavra) 
+                {
+                    break;
+                }
+            }
+            if (tam == tamPalavra) 
+            {
+                strcat(dicionario[quantPalavras], buffer);
+                quantPalavras++;
+            }
+        }
+        arquivo.close();
+    }
+    return quantPalavras;
+}
+
+void selecionarPalavra(char palavraResposta[], int qtdPalavras, char dicionario [][tamanhoPalavraMaxima])
+{
+    random_device semente;
+    mt19937 gerador(semente());
+    uniform_int_distribution distribuicao(1,qtdPalavras);
+    int indicePalavra = distribuicao(gerador);
+    strcat(palavraResposta,dicionario[indicePalavra]);
+    return;
+}
+int main()
+{
+    char dicionario[qtdPalavraMaxima][tamanhoPalavraMaxima];
+    int qtdPalavras = criarDicionario(dicionario, tamanhoPalavra);
+    char palavraResposta [tamanhoPalavraMaxima];
+    selecionarPalavra(palavraResposta, qtdPalavras, dicionario);
+    cout<<palavraResposta<<endl;
+}
